21a7043ded495dc0767ffd345d45310df25a2494
Yuhan Yao <yuhanyao@Yuhans-MacBook-Pro.local>
2019-02-23 20:56:48 -0800

    add init.py

---
+++ __init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 27 17:11:41 2018
+
+@author: yuhanyao
+"""
+import os
+
+__all__ = ['force_lc',
+           'compare_lc',
+           'refine_coo',
+           'phot_class',
+           'keypairs']
