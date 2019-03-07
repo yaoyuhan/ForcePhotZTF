333e94f70f01a4a6328ff20f31bd0f953ceb0fc3
Yuhan Yao <yuhanyao@dhcp-194-201.caltech.edu>
2019-03-07 10:25:52 -0800

    private computer

--- private.py
+++ private.py
@@ -4,8 +4,20 @@
 Created on Thu Mar  7 10:13:18 2019
 
 @author: yuhanyao
+
+It is so dumb that Caltech private computer does not have iPython!
 """
+import sys
+import argparse
+
 
 def add_function(a, b):
     return a+b
+
+
+if __name__ == '__main__':
     
+    a = sys.argv[1]
+    b = sys.argv[2]
+    result = add_function(a, b)
+    print (result)
